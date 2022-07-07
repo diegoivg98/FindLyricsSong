a2cf7a7531254c20c00f0e331bb9f784bc778fd5
Diego Vistoso <68931257+diegoivg98@users.noreply.github.com>
2022-07-07 01:22:50 -0400

    Update main.js
    agregar efecto spinner mientras realiza fetch a la API

--- main.js
+++ main.js
@@ -3,17 +3,20 @@ const song = document.getElementById("song");
 const lyrics = document.getElementById("lyrics");
 const getsong = document.getElementById("getsong");
 
-getsong.addEventListener("click", () => {
+document.getElementById("getsong").addEventListener("click", function () {
   artist.innerHTML = artist.value;
   song.innerHTML = song.value;
-
-  async function getLyrics() {
-    const response = await fetch(
-      `https://api.lyrics.ovh/v1/${artist.value}/${song.value}`
-    );
-    const data = await response.json();
-    console.log(data.lyrics);
-    lyrics.innerHTML = data.lyrics.replace(new RegExp("\n", "g"), "<br>");
-  }
-  getLyrics();
+  document.getElementById("lyrics").innerHTML = '<div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>';
+  setTimeout(function () {
+    async function getLyrics() {
+      const response = await fetch(
+        `https://api.lyrics.ovh/v1/${artist.value}/${song.value}`
+      );
+      const data = await response.json();
+      document.getElementById("lyrics").innerHTML = data.lyrics.replace(
+        new RegExp("\n", "g"),"<br>"
+      );
+    }
+    getLyrics();
+  }, 2000);
 });
