7927f042fcb4e0361d06a61c350f521ab4de2de9
Diego Vistoso <68931257+diegoivg98@users.noreply.github.com>
2022-07-14 16:59:43 -0400

    Update main.js
    agregar funcion para que los inputs tengan la primera letra en mayuscula

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const artist = document.getElementById("artist");
 const song = document.getElementById("song");
 const lyric = document.getElementById("lyrics");
@@ -8,40 +7,59 @@ getsong.addEventListener("click", function (e) {
   e.preventDefault();
   artist.innerHTML = artist.value;
   song.innerHTML = song.value;
-
   if (artist.value === "" || song.value === "") {
     mostrarError("Ambos campos son obligatorios");
     return;
   }
 
+  function capitalizarPrimeraLetra() {
+	var p1 = artist.value;
+	var p2 = song.value;
+	if(!artist.value || !song.value) return;
+	var mayuscula = p1.substring(0,1).toUpperCase();
+	var mayuscula2 = p2.substring(0,1).toUpperCase();
+	if (p1.length > 0 || p2.lenght > 0) {
+	  var minuscula = p1.substring(1).toLowerCase();
+	  var minuscula2 = p2.substring(1).toLowerCase();
+	}
+	artist.value = mayuscula.concat(minuscula);
+	song.value = mayuscula2.concat(minuscula2);
+  }
+
   setTimeout(function () {
-    lyric.innerHTML = '<div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>';
-    getSong();
-  },2000);
+	capitalizarPrimeraLetra();
+	console.log(artist.value);
+	console.log(song.value);
+    lyric.innerHTML =
+      '<div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>';
+    getSong(artist.value, song.value);
+  }, 2000);
 });
 
-function getSong(){
-  fetch(`https://api.lyrics.ovh/v1/${artist.value}/${song.value}`)
-  .then(response => response.json())
-  .then(resultado => {
-    console.log(resultado.lyrics);
-    if (resultado.lyrics) {
-      lyric.innerHTML = resultado.lyrics.replace(new RegExp("\n", "g"),"<br>");
-    } else {
-      lyric.innerHTML =  ''
-      mostrarError("La cancion no existe...");
-    }
-})
+function getSong(artist, song) {
+  fetch(`https://api.lyrics.ovh/v1/${artist}/${song}`)
+    .then((response) => response.json())
+    .then((resultado) => {
+      console.log(resultado.lyrics);
+      if (resultado.lyrics) {
+        lyric.innerHTML = resultado.lyrics.replace(
+          new RegExp("\n", "g"),
+          "<br>"
+        );
+      } else {
+        lyric.innerHTML = "";
+        mostrarError("La cancion no existe...");
+      }
+    });
 }
 
-function mostrarError(mensaje){
+function mostrarError(mensaje) {
   const error = document.createElement("p");
   error.classList.add("error-mensaje");
   error.innerText = mensaje;
 
   lyric.appendChild(error);
   setTimeout(() => {
-      error.remove();
+    error.remove();
   }, 3000);
 }
-
